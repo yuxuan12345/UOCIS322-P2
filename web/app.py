d447aca235ac29da6a96dcffb4da58c4a27b90fd
yuxuan <yuxzhang@qq.com>
2021-04-18 15:04:16 -0700

    updated

--- web/app.py
+++ web/app.py
@@ -2,13 +2,32 @@
 John Doe's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, send_from_directory, abort
 
 app = Flask(__name__)
 
+CAT = """
+     ^ ^
+   =(   )=
+"""
+
 @app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+def cat():
+    return CAT
+
+@app.route("/<path:file>")
+def OK(file):
+    if (("//" in file) or ("~" in file) or (".." in file)):
+        abort(403)
+    return send_from_directory("./", file)
+
+@app.errorhandler(403)
+def error_403(e):
+    return send_from_directory("./", "403.html"), 403
+
+@app.errorhandler(404)
+def error_404(e):
+    return send_from_directory("./", "404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
